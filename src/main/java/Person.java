17b4a267fd1632ac7447b77a16605d8039e6cebe
Tomasz Kordiak <tkordiak@gmail.com>
2021-07-02 17:20:31 +0200

    Person.java and ProducerPerson.java added

---
+++ src/main/java/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    String firstName;
+    String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
