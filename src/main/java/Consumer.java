b0b6db5cc34c50eed23094a3af61f75913e8dc52
Tomasz Kordiak <tkordiak@gmail.com>
2021-07-03 13:11:33 +0200

    Consumer.java added

---
+++ src/main/java/Consumer.java
@@ -0,0 +1,20 @@
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class Consumer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092, localhost:9093, localhost:9094");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+
+        String topics[] = {"numbers"};
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe(Arrays.asList(topics));
+    }
+}
